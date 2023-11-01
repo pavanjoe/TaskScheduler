84f2c6bfd600f159c807ee9bc5cf2d715403ada1
Realitylion <dhanushbaalaji123@gmail.com>
2023-11-01 23:16:56 +0530

    feat: create react app

---
+++ src/pages/homepage.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar";
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar />
+            <div>homepage</div>
+        </div>
+    );
+}
+
+export default Homepage;
