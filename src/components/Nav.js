6b43ef7ad01b7f12afced810ecc43e0ade57c7d2
Realitylion <dhanushbaalaji123@gmail.com>
2023-11-08 21:53:42 +0530

    responsiveness: navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -44,58 +44,61 @@ function Nav() {
           }
         `}
       </style>
-      <nav className="navbar navbar-expand-lg py-2">
-        <span className="navbar-brand mx-4" onClick={() => {handleLogoClick()}} style={{cursor: "pointer"}}>Planify</span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <span className="nav-link mx-3" onClick={() => {navigate("/about")}} style={{cursor: "pointer"}}>
-                About Us
-              </span>
-            </li>
-            <li className="nav-item">
-              <span className="nav-link mx-3" onClick={() => {navigate("/contact")}} style={{cursor: "pointer"}}>
-                Contact Us
-              </span>
-            </li>
-          </ul>
-        </div>
-        
-        {currentUser &&
-          <div className='dropdown me-3'>
-            <div id="profileIconDropdown" data-bs-toggle="dropdown" aria-expanded="false"
-            style={{cursor: "pointer"}}>
-              <IconContext.Provider value={{ color: "white", size: "50px", className: "user-icon"}}>
-                <FaUserCircle />
-              </IconContext.Provider>
-            </div>
-            
-            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="profileIconDropdown">
-              <li className="dropdown-item" onClick={() => {navigate("/profile")}} 
-              style={{cursor: "pointer", userSelect: "none"}}>Profile</li>
-              <li className="dropdown-item" onClick={(e) => {handleSignOut(e)}} 
-              style={{cursor: "pointer", userSelect: "none"}}>Logout</li>
-            </ul> 
-          </div>
-        }
+      <nav className="navbar navbar-expand-sm py-2">
+        <div className='container-fluid d-flex justify-content-between'>
+          <span className="navbar-brand mx-4 order-0 flex-sm-grow-0 flex-grow-1" onClick={() => {handleLogoClick()}} style={{cursor: "pointer"}}>Planify</span>
+          
+          <button 
+            class="navbar-toggler mx-3" 
+            type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarTogglerDemo03" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        {!currentUser &&
-        <div className="ml-auto">
-          <button className="btn btn-danger me-4"
-          onClick={() => {navigate("/login")}} >Login</button>
+          <div className="collapse navbar-collapse order-sm-1 order-2" id="navbarNav">
+            <ul className="navbar-nav ml-auto d-flex align-items-start">
+              <li className="nav-item">
+                {/* About us is disabled temporarily */}
+                <span className="nav-link mx-3 disabled text-secondary" onClick={() => {navigate("/about")}} style={{cursor: "pointer"}}>
+                  About Us
+                </span>
+              </li>
+              <li className="nav-item">
+                <span className="nav-link mx-3" onClick={() => {navigate("/contact")}} style={{cursor: "pointer"}}>
+                  Contact Us
+                </span>
+              </li>
+            </ul>
+          </div>
+          
+          {currentUser &&
+            <div className='dropdown me-3 order-sm-2 order-1'>
+              <div id="profileIconDropdown" data-bs-toggle="dropdown" aria-expanded="false"
+              style={{cursor: "pointer"}}>
+                <IconContext.Provider value={{ color: "white", size: "50px", className: "user-icon"}}>
+                  <FaUserCircle />
+                </IconContext.Provider>
+              </div>
+              
+              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="profileIconDropdown">
+                <li className="dropdown-item" onClick={() => {navigate("/profile")}} 
+                style={{cursor: "pointer", userSelect: "none"}}>Profile</li>
+                <li className="dropdown-item" onClick={(e) => {handleSignOut(e)}} 
+                style={{cursor: "pointer", userSelect: "none"}}>Logout</li>
+              </ul> 
+            </div>
+          }
+
+          {!currentUser &&
+          <div className="ml-auto order-sm-2 order-1">
+            <button className="btn btn-danger me-4"
+            onClick={() => {navigate("/login")}} >Login</button>
+          </div>
+          }
         </div>
-        }
       </nav>
     </>
   );
--- src/pages/page404.js
+++ src/pages/page404.js
@@ -19,7 +19,7 @@ const Page404 = () => {
         <div className="container text-left text-white display-3" style={{paddingTop: "25vh"}}>
             <p className="pb-3">Something's wrong here...</p>
             <h1>We can't find the page you're looking for.</h1>
-            <button className="px-3 py-2 bg-white border-white" onClick={(e) => handleClick(e)}
+            <button className="px-3 py-2 bg-white btn" onClick={(e) => handleClick(e)}
             style={{fontSize: "16px", color: "#1f3345"}}>Back To Home</button>
         </div>
     )
