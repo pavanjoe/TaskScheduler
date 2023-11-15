35ee16749b5c7b50c00258ba968b5ad3b9a7c5d9
pavanjoe <joe.pavan@gmail.com>
2023-11-15 20:39:42 +0530

    added create and delete button

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -86,151 +86,139 @@ const Homepage = () => {
         }
     ];
 
-    return(
+    return (
         <>
-            <Nav />
-            <div className="homepage-wrapper pb-5">
-                <div className="container-fluid">
-                    <div className="row d-flex flex-column align-items-center mt-2">
-                        <div className="btn-group">
-                            <div
-                                className="dropdown-toggle  m-3"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Views
-                            </div>
-                            <div className="dropdown-menu">
-                                <button
-                                className="dropdown-item"
-                                onClick={() => {
-                                    setView("list");
-                                }}
-                                >
-                                List View
-                                </button>
-                                <button
-                                className="dropdown-item"
-                                onClick={() => {
-                                    setView("grid");
-                                }}
-                                >
-                                Grid View
-                                </button>
-                                <button
-                                className="dropdown-item"
-                                onClick={() => {
-                                    setView("calendar");
-                                }}
-                                >
-                                Calendar View
-                                </button>
-                            </div>
-                            <div
-                                className="dropdown-toggle m-3"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Sort by
-                            </div>
-                            <div className="dropdown-menu">
-                                <button
-                                className="dropdown-item"
-                                >
-                                Alphabetical
-                                </button>
-                                <button
-                                className="dropdown-item"
-                                >
-                                Created date
-                                </button>
-                                <button
-                                className="dropdown-item"
-                                >
-                                Deadline
-                                </button>
-                            </div>
-                            <div
-                                className="dropdown-toggle m-3"
-                                data-bs-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                            >
-                                Filter by
-                            </div>
-                            <div className="dropdown-menu">
-                                <button
-                                className="dropdown-item"
-                                >
-                                Category
-                                </button>
-                                <button
-                                className="dropdown-item"
-                                >
-                                Priority
-                                </button>
-                            </div>
-                        </div>
+          <Nav />
+          <div className="homepage-wrapper pb-5">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-9">
+                  <div className="btn-group">
+                    <div
+                      className="dropdown-toggle m-3"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Views
                     </div>
+                    <div className="dropdown-menu">
+                      <button
+                        className="dropdown-item"
+                        onClick={() => {
+                          setView("list");
+                        }}
+                      >
+                        List View
+                      </button>
+                      <button
+                        className="dropdown-item"
+                        onClick={() => {
+                          setView("grid");
+                        }}
+                      >
+                        Grid View
+                      </button>
+                      <button
+                        className="dropdown-item"
+                        onClick={() => {
+                          setView("calendar");
+                        }}
+                      >
+                        Calendar View
+                      </button>
+                    </div>
+    
+                    <div
+                      className="dropdown-toggle m-3"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Sort by
+                    </div>
+                    <div className="dropdown-menu">
+                      <button className="dropdown-item">Alphabetical</button>
+                      <button className="dropdown-item">Created date</button>
+                      <button className="dropdown-item">Deadline</button>
+                    </div>
+    
+                    <div
+                      className="dropdown-toggle m-3"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Filter by
+                    </div>
+                    <div className="dropdown-menu">
+                      <button className="dropdown-item">Category</button>
+                      <button className="dropdown-item">Priority</button>
+                    </div>
+                  </div>
                 </div>
-                { view==="grid" && 
-                <div className="container-fluid grid-view">
-                    <div className="row">
-                        { tasks.map((task) => (
-                            <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2" key={task.id}>
-                                <div className="card my-3">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{task.name}</h5>
-                                        <p className="card-text">{task.description}</p>
-                                        <button className="btn btn-primary">View</button>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>}
-                {view === "list" && (
-                <div className="container-fluid list-view">
-                    <div className="row d-flex flex-column align-items-center">
-                    {tasks.map((task) => (
-                        <div className="col-8 m-3" key={task.id}>
-                        <div className="card">
-                            <div className="card-body d-flex justify-content-between align-items-center">
-                            <div style={{width:"70%"}}>
-                            <h5 className="">{task.name}</h5>
-                            </div>
-                            <div>
-                            <span className={`priority-tag priority-${task.priority.toLowerCase()} me-2`}>
-                            {task.priority.charAt(0).toUpperCase() + task.priority.slice(1).toLowerCase()}
-                            </span>
-                            <button className="btn btn-primary">View</button>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    ))}
-                    </div>
+                <div className="col-md-3 d-flex align-items-center justify-content-end">
+                  <button className="btn btn-success m-3">Create Task</button>
                 </div>
-                )}
-                { view==="calendar" &&
-                <div className="container-fluid">
-                    <div className="row d-flex flex-column align-items-center">
-                        <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12">
-                            <div className="card">
-                                <div className="card-body">
-                                    Calendar
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                }
+              </div>
             </div>
-            <Footer />
+            {view === "grid" && (
+              <div className="container-fluid grid-view">
+                <div className="row">
+                  {tasks.map((task) => (
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2" key={task.id}>
+                      <div className="card my-3">
+                        <div className="card-body">
+                          <h5 className="card-title">{task.name}</h5>
+                          <p className="card-text">{task.description}</p>
+                          <button className="btn btn-primary me-2">View</button>
+                          <button className="btn btn-danger">Delete</button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {view === "list" && (
+              <div className="container-fluid list-view">
+                <div className="row d-flex flex-column align-items-center">
+                  {tasks.map((task) => (
+                    <div className="col-8 m-3" key={task.id}>
+                      <div className="card">
+                        <div className="card-body d-flex justify-content-between align-items-center">
+                          <div style={{ width: "70%" }}>
+                            <h5 className="d-inline me-3">{task.name}</h5>
+                            <span className={`priority-tag priority-${task.priority.toLowerCase()} me-2`}>
+                              {task.priority.charAt(0).toUpperCase() + task.priority.slice(1).toLowerCase()}
+                            </span>
+                          </div>
+                          <div>
+                            <button className="btn btn-primary me-2">View</button>
+                            <button className="btn btn-danger">Delete</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {view === "calendar" && (
+              <div className="container-fluid">
+                <div className="row d-flex flex-column align-items-center">
+                  <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12">
+                    <div className="card">
+                      <div className="card-body">Calendar</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <Footer />
         </>
-    );
-}
-
-export default Homepage;
+      );
+    };
+    
+    export default Homepage;
