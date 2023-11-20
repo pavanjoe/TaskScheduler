ef1e5316ac8805ae915e3365ef5a9d1775206d8e
pavanjoe <joe.pavan@gmail.com>
2023-11-20 11:21:20 +0530

    changed to previous email

--- src/pages/contactpage.js
+++ src/pages/contactpage.js
@@ -19,7 +19,7 @@ const Contactpage = () => {
     e.preventDefault();
 
     emailjs
-     .sendForm('service_22o6fqg', 'template_3dpr6aw', form.current, 'cvHLMLLH36YW5TXhi')
+     .sendForm('service_vt5zhrg', 'template_3dpr6aw', form.current, 'cvHLMLLH36YW5TXhi')
         .then((result) => {
             console.log(result.text);
             console.log("message sent");
@@ -34,14 +34,12 @@ const Contactpage = () => {
             <div className="contact-form">
                 <div className="container">
                     <form ref={form} onSubmit={sendEmail}>
-                        <h1 style={{color:"#faebd7",fontSize:"40px",marginLeft:"-4px"}}>Contact <span>Us</span> </h1>
-                        <br />
-                        <input type="text" name="name" onChange={handleChange} value={data.name} placeholder="Enter your name" autoComplete="off" required />
-                        <input type="email" name="email" onChange={handleChange} value={data.email} placeholder="Enter your email" autoComplete="off" required />
-                        <input type="tel" name="phone"   onChange={handleChange} value={data.phone} placeholder="Enter your phone no." autoComplete="off" required />
-                        <br />
-                        <textarea name="message" rows="10" cols="40"  onChange={handleChange} value={data.message} placeholder="type your message... " autoComplete="off" required/>
-                       <center> <button type="submit">Submit</button></center>
+                        <h1>Contact <span>Us</span> </h1>
+                        <input type="text" name="name" onChange={handleChange} value={data.name} placeholder="Enter your name" autoComplete="off" required/>
+                        <input type="email" name="email" onChange={handleChange} value={data.email} placeholder="Enter your email" autoComplete="off" required/>
+                        <input type="tel" name="phone"   onChange={handleChange} value={data.phone} placeholder="Enter your phone no." autoComplete="off" required/>
+                        <textarea name="message" rows="10" cols="30"  onChange={handleChange} value={data.message} placeholder="type your message... " autoComplete="off" required/>
+                        <button type="submit">Submit</button>
                     </form>
                 </div>
             </div>
